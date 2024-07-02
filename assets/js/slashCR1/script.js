7e252e82d39f19c18599154274b8c22bed11df68
D <DavidCoats64@gmail.com>
2024-07-01 22:29:16 -0700

    add playerScoreNum++ to increase the player score and a line to display to canvas

--- assets/js/slashCR1/script.js
+++ assets/js/slashCR1/script.js
@@ -1,5 +1,25 @@
+//Create Const for player's Score
+const playerScore = document.getElementById('playerScore');
+
+//Create var to track player's Score number
+let playerScoreNum = 0;
+
 function startGame() {
 	alert('Are you sure you want to play a new Game')
 
 }
 
+function resetPlayerScore() {
+
+	playerScoreNum = 0;
+	playerScore.textContent = playerScoreNum;
+
+
+}
+
+function updateScore(){
+
+	playerScoreNum++;
+	playerScore.textContent = playerScoreNum;
+
+}
