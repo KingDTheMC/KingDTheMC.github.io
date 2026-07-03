33066d8ab7ce057d42bcae82c30970177cdbac28
D <DavidCoats64@gmail.com>
2026-07-03 08:20:54 -0700

    clean up unused code

--- assets/js/kingdthemc.js
+++ assets/js/kingdthemc.js
@@ -29,41 +29,7 @@ if (btn){
   setTheme(getInitialTheme());
 //})();
 //</script>
-//Edited out old js for night mode
-//creating dark theme toggle switch
-/*
-const toggleButton = document.getElementById('dark-mode-toggle');
-const body = document.body;
 
-//Function to enable dark mode
-	function enableDarkMode() {
-		body.classList.add('dark-mode');
-		localStorage.setItem('theme', 'dark');
-	}
-
-//Function to dsiable dark mode
-	function disableDarkMode() {
-		body.calssList.remove('dark-mode');
-		localStorage.setItem('theme', 'light');
-	}
-
-//Check for saved theme on page load
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-	enableDarkMode();
-} else {
-	disableDarkMode(); //Ensure light mode is active if no preference
-}
-
-//Add event listener to the button
-toggleButton.addEventListener('click', () => {
-	if (body.classList.contains('dark-mode')) {
-		disableDarkMode();
-	} else {
-		enableDarkMode();
-	}
-});
-*/
 
 //Copy to clipboard fucntion
 	function copyToClipboard(element) {
@@ -77,44 +43,3 @@ toggleButton.addEventListener('click', () => {
 
 
 
-
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
